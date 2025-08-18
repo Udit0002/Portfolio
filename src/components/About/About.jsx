12f0e85f057ab16fcd6114928862ddd4639adc81
Udit Chaudhary <uditchaudhary@Mac.lan>
2025-08-19 01:33:24 +0530

    Updated About section with responsive resume button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,18 +42,19 @@ const About = () => {
             Full-stack developer specializing in the MERN stack, Next.js, and scalable cloud solutions — delivering seamless user experiences with high-performance web apps.
           </p>
           {/* Resume Button */}
-          {/* <a
+          <a
             href="https://drive.google.com/file/d/1j4ADLBAVzWRWM4JrkkVv0bRq2Ng-BlYq/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 block md:hidden"
             style={{
               background: 'linear-gradient(90deg, #8245ec, #a855f7)',
               boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
             }}
           >
             DOWNLOAD CV
-          </a> */}
+          </a>
+
           
         </div>
         {/* Right Side */}
